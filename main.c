725d165af62c2bbb59357be8c8cb5697d994301d
Elbadry2025 <96208963+Elbadry2025@users.noreply.github.com>
2021-12-28 23:07:18 +0200

    fixing grid

--- main.c
+++ main.c
@@ -12,13 +12,14 @@
 
 
 void print(int size1,int arr1[][size1]){
-    printf("\n\t\t\t\t\t\t   ");
+    printf("\n\t\t\t\t\t\t    "); //
     for(int j=1 ;j<size1 ;j++){
         printf(" %d",arr1[0][j]);
     }
     printf("\n");
     for(int i=1 ; i<size1 ; i++){
-        printf("\t\t\t\t\t\t   %d",i);
+            if(i>9){printf("\t\t\t\t\t\t   %d",i);} /////
+        else{printf("\t\t\t\t\t\t   %d ",i);} /////
         for(int j=1; j<size1 ;j++){
             if( arr1[i][j]==254){
                printf( ANSI_COLOR_WHITE   "%c " ANSI_COLOR_RESET ,arr1[i][j] );
